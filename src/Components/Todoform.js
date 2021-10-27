fc42e11535027cd1bad8f0601ebab348f2f17b6d
mathieuelisa <mathieu.elisa@gmail.com>
2021-10-27 09:42:13 +0100

    Update CSS and refactornig code

--- src/Components/Todoform.js
+++ src/Components/Todoform.js
@@ -1,7 +1,9 @@
 import { useState, useRef, useEffect } from "react";
+// Import React Reveal Effect
+import Fade from "react-reveal/Fade";
 
-function TodoForm(props) {
-  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+function TodoForm({ edit, onSubmit }) {
+  const [input, setInput] = useState(edit ? edit.value : "");
 
   const inputRef = useRef();
 
@@ -13,11 +15,10 @@ function TodoForm(props) {
   function handleSubmit(e) {
     e.preventDefault();
 
-    props.onSubmit({
+    onSubmit({
       id: Math.floor(Math.random() * 200000),
       text: input,
     });
-
     setInput("");
   }
 
@@ -26,35 +27,37 @@ function TodoForm(props) {
   }
 
   return (
-    <form className="todo__form" onSubmit={handleSubmit}>
-      {props.edit ? (
-        <>
-          <input
-            type="text"
-            placeholder="Update your task"
-            value={input}
-            name="text"
-            className="todo__form-input"
-            onChange={handleChange}
-            ref={inputRef}
-          />
-          <button className="todo__form-button">Update your task</button>
-        </>
-      ) : (
-        <>
-          <input
-            type="text"
-            placeholder="Add a task"
-            value={input}
-            name="text"
-            className="todo__form-input"
-            onChange={handleChange}
-            ref={inputRef}
-          />
-          <button className="todo__form-button">Add a new task</button>
-        </>
-      )}
-    </form>
+    <Fade left duration={2000}>
+      <form className="todo__form" onSubmit={handleSubmit}>
+        {edit ? (
+          <>
+            <input
+              type="text"
+              placeholder="Update your task"
+              value={input}
+              name="text"
+              className="todo__form-input"
+              onChange={handleChange}
+              ref={inputRef}
+            />
+            <button className="todo__form-button">Update your task</button>
+          </>
+        ) : (
+          <>
+            <input
+              type="text"
+              placeholder="Add a task"
+              value={input}
+              name="text"
+              className="todo__form-input"
+              onChange={handleChange}
+              ref={inputRef}
+            />
+            <button className="todo__form-button">Add a new task</button>
+          </>
+        )}
+      </form>
+    </Fade>
   );
 }
 
